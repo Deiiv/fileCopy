3881889bbfdf3a3cd8416478b40b319fd89918cc
David Fedorovsky <david.fedorovsky@gmail.com>
2022-07-15 00:14:13 -0400

    Adding slash to copy contents, without 'source'

--- copy.sh
+++ copy.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "$(date) - Starting copy job with"
 
-rsync -a -v --ignore-existing --exclude ".*/" --exclude '*.tmp*' /source /destination
+rsync -a -v --ignore-existing --exclude ".*/" --exclude '*.tmp*' /source/ /destination
 
 echo "$(date) - End of copy job"
