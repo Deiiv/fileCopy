d131f46ac8f0fa9a65918b2760ff7cac768b2dc8
David Fedorovsky <david.fedorovsky@gmail.com>
2023-05-28 09:54:04 -0400

    Adding ignor .tmp

--- copy.sh
+++ copy.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "$(date) - Starting copy job with"
 
-rsync -a -v --ignore-existing --exclude '.*/' --exclude '*.tmp*' --exclude '*/Sent/' /source/ /destination && mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PW -t $MQTT_TOPIC -m "Healthy as of $(date '+%d-%m-%Y')"
+rsync -a -v --ignore-existing --exclude '.*/' --exclude '*.tmp*' --exclude '*.tmp' --exclude '*/Sent/' /source/ /destination && mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PW -t $MQTT_TOPIC -m "Healthy as of $(date '+%d-%m-%Y')"
 
 echo "$(date) - End of copy job"
